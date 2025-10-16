e30e2d6ad35bb8d63b9fa2fd86f63e4b06e5ef2f
Julio Saráuz <jsarauz@29deoctubre.fin.ec>
2025-10-16 10:19:03 -0500

    mejoras

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,13 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { ComponentesModule } from '../componentes/componentes.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ComponentesModule
   ],
   declarations: [HomePage]
 })
